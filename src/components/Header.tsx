1d0ab7915620bf7d512e61f6e21e53d15367782c
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-03-10 21:27:06 -0700

    feat:add about and resume link in nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ function Header() {
             <div className="logo">RB<span className="logo dot">.</span></div>
             <nav>
                 <ul>
+                    <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
+                    <li><a href="https://docs.google.com/document/d/14cHd1mq0Y5ZafeT8rRrrmRGByKgCA3Ut/edit?usp=sharing&ouid=107965096588025037128&rtpof=true&sd=true" target="_blank" rel="noreferrer">Resume</a></li>
                 </ul>
             </nav>
         </header>
