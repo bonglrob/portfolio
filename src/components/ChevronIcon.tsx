5c9240c930953cf29d8fd993df5db516dc4c5b05
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-03-10 21:04:59 -0700

    add chevron icon to hero banner to direct users to scroll down

---
+++ src/components/ChevronIcon.tsx
@@ -0,0 +1,20 @@
+function ChevronIcon() {
+    return (
+        <a href="#about" className="scroll-prompt">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                width="24"
+                height="24"
+                strokeWidth="2"
+            >
+                <path d="M7 7l5 5l5 -5"></path>
+                <path d="M7 13l5 5l5 -5"></path>
+            </svg>
+        </a>
+    );
+}
+
+export default ChevronIcon;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import waves from '/video/waves.mp4';
 import Header from "./Header";
+import ChevronIcon from "./ChevronIcon";
 
 function Hero() {
     return (
@@ -9,6 +10,8 @@ function Hero() {
                 <h1 className="container">Robert Bonglamphone</h1>
                 <h2 className="container">Full-Stack Software Developer</h2>
             </div>
+            <ChevronIcon />
+            {/* <img className="scroll-prompt" width="50" height="50" src="https://img.icons8.com/ios/50/expand-arrow--v1.png" alt="expand-arrow--v1"/> */}
             <video id="particles-video" className="video-bg" autoPlay muted loop>
                 <source src={waves} type="video/mp4"/>
             </video>
